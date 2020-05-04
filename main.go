7747b7c4995b4da395c8eae21823142f76fb0888
Sunao Uehara <uehara.sunao@gmail.com>
2020-05-04 16:22:10 -0700

    test file path from modules

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package modtest
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+)
 
 func Print() {
 	fmt.Println("v1.0.1")
+}
+
+func PrintPath() {
+	abs, err := filepath.Abs("./data/")
+	if err == nil {
+		fmt.Println("Absolute:", abs)
+	}
 }
