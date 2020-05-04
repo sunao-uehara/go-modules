6ba8bcaf278fd647479dba4310e44b8d5341366b
Sunao Uehara <uehara.sunao@gmail.com>
2020-05-04 16:38:48 -0700

    test file path from modules

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package modtest
 
 import (
 	"fmt"
+	"os"
 	"path"
 	"path/filepath"
 	"runtime"
@@ -21,4 +22,18 @@ func PrintPath() {
 	if ok {
 		fmt.Println("filename:", path.Dir(filename))
 	}
+
+	// from Executable Directory
+	ex, _ := os.Executable()
+	fmt.Println("Executable DIR:", filepath.Dir(ex))
+
+	// Current working directory
+	dir, _ := os.Getwd()
+	fmt.Println("CWD:", dir)
+
+	// Relative on runtime DIR:
+	_, b, _, _ := runtime.Caller(0)
+	d1 := path.Join(path.Dir(b))
+	fmt.Println("Relative", d1)
+
 }
